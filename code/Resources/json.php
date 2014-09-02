f9f2cfa268024388e4083cfd685aa13c798c3d65
johnnyeven_work <johnnyeven@gmail.com>
2014-09-02 18:10:36 +0800

    1

--- code/Resources/json.php
+++ code/Resources/json.php
@@ -1,11 +1,29 @@
 <?php
 $config = array(
 	'mapPListFilePath'				=>	'images/maps/A-1-2.plist',
-	'spriteFrameName'				=>	'A-1-2.png',
+	'mapSpriteFrameName'			=>	'A-1-2.png',
+	'stationPListFilePath'			=>	'images/maps/stations/A-1-2.plist',
+	'starPListFilePath'				=>	'images/maps/stars/A-1-2.plist',
 	'stations'						=>	array(
 		array(
-			'resource'				=>	'images/stations/space_station_blue.png',
-			'zIndex'				=>	0.01,
+			'resource'				=>	'star_0.png',
+			'zIndex'				=>	0.05,
+			'blockX'				=>	1,
+			'blockY'				=>	0,
+			'positionX'				=>	10500,
+			'positionY'				=>	2000
+		),
+		array(
+			'resource'				=>	'star_1.png',
+			'zIndex'				=>	0.03,
+			'blockX'				=>	1,
+			'blockY'				=>	0,
+			'positionX'				=>	10500,
+			'positionY'				=>	2000
+		),
+		array(
+			'resource'				=>	'space_station_blue.png',
+			'zIndex'				=>	0.02,
 			'blockX'				=>	1,
 			'blockY'				=>	0,
 			'positionX'				=>	10500,
